67eeb2aaa94be9bcfc2429d40020b43fa2611589
mayurkhatri <mayurkhatri122@gmail.com>
2015-01-26 12:45:06 +0530

    Pre Heroku Push

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'home/welcome'
+
   get 'shows/index'
 
   get 'shows/show'
@@ -21,7 +23,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'recipes#new'
+  root 'home#welcome'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
